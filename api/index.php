92a6da915a345130c6251aaa5d0781ec7dcbe621
FendyK14 <fendyk1706@gmail.com>
2024-06-08 21:07:58 +0700

    VERCEL

--- api/index.php
+++ api/index.php
@@ -1,20 +1,3 @@
-<?php
+<?php  
 
-require __DIR__ . '/../vendor/autoload.php';
-
-// Load the environment variables from the .env file
-(Dotenv\Dotenv::createImmutable(__DIR__ . '/../'))->load();
-
-// Create the application
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Handle the request
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . "/../public/index.php";
